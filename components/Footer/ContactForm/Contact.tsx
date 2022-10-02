263cadc26892c432e07b2d4b0188440721305c23
Baptiste TRAUTMANN <baptistetrautmann@hotmail.fr>
2022-10-03 01:51:35 +0200

    feat: developed mail sender for contact form
    Signed-off-by: Baptiste TRAUTMANN <baptistetrautmann@hotmail.fr>

--- components/Footer/ContactForm/Contact.tsx
+++ components/Footer/ContactForm/Contact.tsx
@@ -1,9 +1,11 @@
-import React, {FunctionComponent, useContext} from 'react';
+import React, {FunctionComponent, useContext, useState} from 'react';
 import style from './Contact.module.css';
 import {locales, LocalisationContext} from "../../../contexts/Locale";
 
 type ContactProps = {
 }
+
+
 
 const resetInput = (e: React.FocusEvent<HTMLTextAreaElement | HTMLInputElement>, defaultValue: string) => {
     if(e && e.target.value === defaultValue) {
@@ -17,17 +19,54 @@ const resetLabels = (e: React.FocusEvent<HTMLTextAreaElement | HTMLInputElement>
     }
 }
 
+
+
 export const Contact: FunctionComponent<ContactProps> = ({}) => {
+
+    const [name, setName] = useState('')
+    const [email, setEmail] = useState('')
+    const [message, setMessage] = useState('')
+    const [submitted, setSubmitted] = useState(false)
+
+    const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+        e.preventDefault();
+        console.log('Sending');
+
+        let data = {
+            name,
+            email,
+            message
+        }
+
+        fetch('/api/contact', {
+            method: 'POST',
+            headers: {
+                'Accept': 'application/json, text/plain, */*',
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify(data)
+        }).then((res) => {
+            console.log('Response received')
+            if (res.status === 200) {
+                console.log('Response succeeded!')
+                setSubmitted(true)
+                setName('')
+                setEmail('')
+                setMessage('')
+            }
+        })
+
+    }
 
     const localisation = useContext(LocalisationContext);
     let title: string = localisation.locale === locales.francais ? "Posez moi vos question!" : "Ask me anything!";
 
     return <div id={style["contactDiv"]}>
-    <form id={style["contactForm"]}>
+    <form id={style["contactForm"]} onSubmit={(e) => {handleSubmit(e)}} >
         <p>{title}</p>
-        <input id={style["contactNameInput"]} type="text" onBlur={(e)=>resetLabels(e, localisation.locale === locales.francais ? "Nom" : "Name")} onFocus={(e) => resetInput(e,localisation.locale === locales.francais ? "Nom" : "Name")} defaultValue={localisation.locale === locales.francais ? "Nom" : "Name"} />
-        <input id={style["contactEmailInput"]} type="text" onBlur={(e)=>resetLabels(e, localisation.locale === locales.francais ? "Adresse email" : "Email adress")} onFocus={(e) => resetInput(e,localisation.locale === locales.francais ? "Adresse email" : "Email adress")}  defaultValue={localisation.locale === locales.francais ? "Adresse email" : "Email adress"} />
-        <textarea id={style["contactMessageInput"]}  name="message" onBlur={(e)=>resetLabels(e, localisation.locale === locales.francais ? "Votre message" : "Your message")} onFocus={(e) => resetInput(e,localisation.locale === locales.francais ? "Votre message" : "Your message")}  defaultValue={localisation.locale === locales.francais ? "Votre message" : "Your message"}></textarea>
+        <input id={style["contactNameInput"]} type="text" onChange={(e)=>{setName(e.target.value)}} onBlur={(e)=>resetLabels(e, localisation.locale === locales.francais ? "Nom" : "Name")} onFocus={(e) => resetInput(e,localisation.locale === locales.francais ? "Nom" : "Name")} defaultValue={localisation.locale === locales.francais ? "Nom" : "Name"} />
+        <input id={style["contactEmailInput"]} type="text" onChange={(e)=>{setEmail(e.target.value)}} onBlur={(e)=>resetLabels(e, localisation.locale === locales.francais ? "Adresse email" : "Email adress")} onFocus={(e) => resetInput(e,localisation.locale === locales.francais ? "Adresse email" : "Email adress")}  defaultValue={localisation.locale === locales.francais ? "Adresse email" : "Email adress"} />
+        <textarea id={style["contactMessageInput"]}  name="message" onChange={(e)=>{setMessage(e.target.value)}} onBlur={(e)=>resetLabels(e, localisation.locale === locales.francais ? "Votre message" : "Your message")} onFocus={(e) => resetInput(e,localisation.locale === locales.francais ? "Votre message" : "Your message")}  defaultValue={localisation.locale === locales.francais ? "Votre message" : "Your message"}></textarea>
         <input id={style["contactSubmit"]} type="submit" value={localisation.locale === locales.francais ? "Envoyer" : "Send"}/>
     </form>
 </div>;
