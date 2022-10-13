31212347bc69178daa39efec1cbdb7d48b541315
Baptiste TRAUTMANN <baptistetrautmann@hotmail.fr>
2022-10-13 11:49:38 +0200

    fix: fixed stories for build
    Signed-off-by: Baptiste TRAUTMANN <baptistetrautmann@hotmail.fr>

--- components/PortfolioSection/PortfolioSection.stories.tsx
+++ components/PortfolioSection/PortfolioSection.stories.tsx
@@ -4,14 +4,22 @@ import {PortfolioSection} from './PortfolioSection';
 import {ComponentMeta, ComponentStory} from '@storybook/react';
 import {withReactContext} from "storybook-react-context";
 import {locales, LocalisationContext} from "../../contexts/Locale";
+import {Description} from "../../pages";
 
 export default {
     title: 'Content/PortfolioSection',
     component: PortfolioSection,
 } as ComponentMeta<typeof PortfolioSection>;
 
-export const Primary: ComponentStory<typeof PortfolioSection> = () => <PortfolioSection projects={[]}/>
-export const English: ComponentStory<typeof PortfolioSection> = () => <PortfolioSection projects={[]}/>
+let description : Description = {
+    id: 1,
+    description_fr: "Description en fr",
+    description_eng: "Description en eng",
+    section_key: "projects"
+}
+
+export const Primary: ComponentStory<typeof PortfolioSection> = () => <PortfolioSection projects={[]} description={description}/>
+export const English: ComponentStory<typeof PortfolioSection> = () => <PortfolioSection projects={[]} description={description}/>
 
 English.decorators = [withReactContext({
     Context: LocalisationContext,
