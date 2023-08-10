785529c2b2792a93c67255ef40a5fb89a0984b7e
Baptiste TRAUTMANN <baptistetrautmann@hotmail.fr>
2023-08-10 17:34:35 +0200

    feat: changement de lib pour le formulaire de mailing
    Signed-off-by: Baptiste TRAUTMANN <baptistetrautmann@hotmail.fr>

--- components/ContactSection/ContactForm/Contact.stories.tsx
+++ components/ContactSection/ContactForm/Contact.stories.tsx
@@ -18,9 +18,10 @@ let description : Description = {
     description_eng: "Description en eng",
     section_key: "contact"
 }
+let email_service = "", email_key = "", email_template_id = "";
 
-export const Primary: ComponentStory<typeof Contact> = () => <Contact description={description}/>
-export const English: ComponentStory<typeof Contact> = () => <Contact description={description}/>
+export const Primary: ComponentStory<typeof Contact> = () => <Contact description={description} email_key={email_key} email_service={email_service} email_template_id={email_template_id}/>
+export const English: ComponentStory<typeof Contact> = () => <Contact description={description} email_key={email_key} email_service={email_service} email_template_id={email_template_id}/>
 
 English.decorators = [withReactContext({
     Context: LocalisationContext,
